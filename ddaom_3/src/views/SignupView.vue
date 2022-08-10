2516d65d298c7a963d61bfadfcd16210bbc7aed0
정혜리 <dmsdl256@naver.com>
2022-08-10 23:47:32 +0900

    signup

--- ddaom_3/src/views/SignupView.vue
+++ ddaom_3/src/views/SignupView.vue
@@ -2,7 +2,7 @@
   <div>
     <title>DDAOM_joinform</title>
     <div class="totalContainer">
-      <h2>DDAOM</h2>
+      <h2><b>DDAOM</b></h2>
       <table class="signupContainer">
         <p></p>
         <tr>
@@ -36,7 +36,10 @@
         <tr>
           <td>
             비밀번호 :
-            <input type="text" name="password" style="margin-left: 8px" />
+            <input v-model="signup.password" type="password" maxlength="16" @blur="passwordValid" style="margin-left: 8px" />
+            <div v-if="!passwordValidFlag">
+              유효하지 않은 비밀번호 입니다.
+            </div>
           </td>
         </tr>
         <p></p>
@@ -45,6 +48,16 @@
             <p style="color: red;">8자 이상 영문 대소문자와 숫자로만 입력해주세요.</p>
           </td>
         </tr>
+        <tr>
+          <td>
+            비밀번호 확인:
+            <input v-model="passwordCheck" type="password" maxlength="16" @blur="passwordCheckValid" style="margin-left: 8px" />
+            <div v-if="!passwordCheckFlag">
+              비밀번호가 동일하지 않습니다.
+            </div>
+          </td>
+        </tr>
+        <p></p>
         <tr>
           <td>
             <button id="signupButton" type="submit" @click="MoveLogin()">
@@ -67,7 +80,14 @@ export default {
   components: {},
   data() {
     return {
-      sampleData: ''
+      signup: {
+        id: null,
+        password: null,
+        pwhint: '',
+        pwhintans: null
+      },
+      passwordCheck: '',
+      passwordValidFlag: true
     }
   },
   setup() {},
@@ -81,6 +101,20 @@ export default {
     },
     MoveBack() {
       this.$router.push('/')
+    },
+    passwordValid () {
+      if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,16}$/.test(this.signup.password)) {
+        this.passwordValidFlag = true
+      } else {
+        this.passwordValidFlag = false
+      }
+    },
+    passwordCheckValid () {
+      if (this.signup.password === this.passwordCheck) {
+        this.passwordCheckFlag = true
+      } else {
+        this.passwordCheckFlag = false
+      }
     }
   }
 }
@@ -111,10 +145,14 @@ input[type='text'] {
   background-color: rgb(238, 235, 235);
   border: 1px solid rgb(172, 171, 171);
 }
+input[type='password'] {
+  background-color: rgb(238, 235, 235);
+  border: 1px solid rgb(172, 171, 171);
+}
 /*button*/
 #signupButton {
   height: 100%;
-  width: 100%;
+  width: 120%;
   float: center;
   background-color: #fcc820;
   color: white;
