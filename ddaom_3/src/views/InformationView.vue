1d198a11011908cdf031a4336d605c48acff8c97
Chaewon <cw001121@naver.com>
2022-08-11 18:10:57 +0900

    fix frame

---
+++ ddaom_3/src/views/InformationView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <frame />
+  </div>
+</template>
+
+<script>
+import Frame from '@/components/Frame.vue'
+
+export default {
+  components: { Frame },
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
+
+<style></style>
