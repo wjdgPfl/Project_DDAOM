fc55db3547b6c412c4cf37f669eaad0373efd356
Chaewon <cw001121@naver.com>
2022-08-31 20:13:52 +0900

    fix : calendar

--- backend/index.js
+++ backend/index.js
@@ -26,25 +26,6 @@ app.post("/api/frame/project_name", async (req, res) => {
   res.send(Project);
 });
 
-app.post("/api/frame/update/checked", async (req, res) => {
-  // content : [0] = 프로젝트 id, [1] = 체크 value
-  const checkValue = req.body.content;
-
-  await database.run(
-    `UPDATE Project_User SET checked = ${checkValue[1]} WHERE id = ${checkValue[0]}`
-  );
-});
-
-app.post("/api/frame/update/color", async (req, res) => {
-  // content : [0] = 프로젝트 id, [1] = 색상 value
-  const colorValue = req.body.content;
-
-  await database.run(
-    `UPDATE Project_User SET color = '${colorValue[1]}' WHERE id = ${colorValue[0]}`
-  );
-  // console.log(req.body.content[1])
-});
-
 // 프레임 끝
 
 // 회원가입 시작
@@ -54,7 +35,7 @@ app.post("/api/signup", async (req, res) => {
     `INSERT IGNORE INTO User (id,name,password,hint) VALUES ('${req.body.content.id}','${req.body.content.name}','${req.body.content.password}','${req.body.content.pwhint}')`
   );
   await database.run(
-    `INSERT INTO Project_User (user_id,user_name,color, checked) VALUES ('${req.body.content.id}','${req.body.content.name}','#000000', FALSE)`
+    `INSERT INTO Project_User (user_id,user_name) VALUES ('${req.body.content.id}','${req.body.content.name}')`
   );
 });
 
@@ -77,30 +58,25 @@ app.post("/api/checkid", async (req, res) => {
 
 app.get("/api/login", async (req, res) => {
   if (req.cookies && req.cookies.token) {
-    // 새로고침해도 로그인 되어있도록
-    // 쿠키에 정보가 있으면
     jwt.verify(req.cookies.token, "46081382", (err, decoded) => {
-      // 쿠키 토큰과 암호가 타당할때
       if (err) {
-        // 쿠키 토큰과 암호가 타당하지 않으면 401
         return res.send(401);
       }
-
-      res.send(decoded); //쿠키의 정보가 맞으면 쿠키토큰 안에있는걸 프론트로 보냄
+      res.send(decoded);
     });
   } else {
-    res.sendStatus(401); // 쿠키가 없으면
+    res.sendStatus(401);
   }
 });
 
 app.post("/api/login", async (req, res) => {
   const members = await database.run("SELECT * FROM User");
 
-  const loginId = req.body.loginId; // 프론트앤드에서 입력받은 아이디
-  const loginpw = req.body.loginPw; //  비밀번호
+  const loginId = req.body.loginId;
+  const loginpw = req.body.loginPw;
   global.a = req.body.loginId;
   const member = members.find(
-    (m) => m.id === loginId && m.password === loginpw // 백앤드에 있는 아디비번과 프론트앤드에서 입력한 아디비번이 일치하면 멤버변수에 넣음
+    (m) => m.id === loginId && m.password === loginpw
   );
 
   if (member) {
@@ -117,28 +93,21 @@ app.post("/api/login", async (req, res) => {
       },
       "46081382",
       {
-        // 쿠키 암호
-        expiresIn: "15m", // 어느정도 쿠키 유지
+        expiresIn: "15m",
         issuer: "ddaom",
       }
     );
-    res.cookie("token", token, option); // 새로고침해도 로그인 안풀리도록 쿠키에 정보 저장
-    // 일치하면 멤버정보를 프론트앤드로 넘겨줌
+    res.cookie("token", token, option);
     res.send(member);
   } else {
-    //일치안하면 404에러
     res.send(404);
   }
 });
 
 app.delete("/api/login", async (req, res) => {
-  
-  await database.run(`UPDATE Project_User SET checked = 0 WHERE user_id = '${a}';`)
-
   if (req.cookies && req.cookies.token) {
     res.clearCookie("token");
   }
-
   res.sendStatus(200);
 });
 
@@ -203,7 +172,7 @@ app.post("/api/makeProject/user", async (req, res) => {
 
 app.post("/api/makeProject/project_user", async (req, res) => {
   await database.run(
-    `INSERT INTO Project_User (user_id,project_id,user_name,checked,color) VALUES ('${req.body.content.user_id}',${req.body.content.id},'${req.body.content.user_name}',FALSE,'#000000')`
+    `INSERT INTO Project_User (user_id,project_id,user_name) VALUES ('${req.body.content.user_id}',${req.body.content.id},'${req.body.content.user_name}')`
   );
 });
 
@@ -212,7 +181,7 @@ app.post("/api/makeProject/project_user/personal", async (req, res) => {
     `SELECT name FROM User WHERE id = '${a}';`
   );
   await database.run(
-    `INSERT INTO Project_User (user_id,project_id,user_name,checked,color) VALUES ('${a}',${req.body.content.id},'${name[0].name}',FALSE,'#000000')`
+    `INSERT INTO Project_User (user_id,project_id,user_name) VALUES ('${a}',${req.body.content.id},'${name[0].name}')`
   );
 });
 
